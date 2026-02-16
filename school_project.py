44bfe87d69e22e4a4b3a9cd905769921a27b7fab
sedemca <sedem@udel.edu>
2026-02-15 23:14:36 -0500

    Create school_project.py with school exercises

---
+++ school_project.py
@@ -0,0 +1,19 @@
+# school_project.py
+
+# School Exercises
+
+def exercise_1():
+    # Your code for exercise 1
+    pass
+
+
+def exercise_2():
+    # Your code for exercise 2
+    pass
+
+
+def exercise_3():
+    # Your code for exercise 3
+    pass
+
+# More exercises can be added similarly.
